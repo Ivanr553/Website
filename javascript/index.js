e5897e2c19422feb109230e5080086b1077f62f0
Ivanr553 <ivanr553@gmail.com>
2017-07-25 16:54:21 -0700

    new website push

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+//Links
+function openCarla() {
+  window.open("http://carlakunkelweb.github.io/website/")
+}
+
+function openRaincheck() {
+  window.open("https://tranquil-chamber-53541.herokuapp.com/")
+}
+
+function openJK() {
+  window.open("./portfolio/index.html")
+}
+
+function openTokiwa() {
+  window.open("./tokiwa/index.html")
+}
